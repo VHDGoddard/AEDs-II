fe2f7e4118ba79cc5e10dd962d20443b77389478
Vgoddard <vinicius.goddard@gmail.com>
2023-08-22 15:39:04 -0300

    commit inicial

---
+++ teste/src/teste/Sandbox.java
@@ -0,0 +1,15 @@
+package teste;
+import java.util.Scanner;
+
+class Sandbox {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		
+		System.out.println(num1 + num2);
+
+		scanner.close();
+	}
+}
