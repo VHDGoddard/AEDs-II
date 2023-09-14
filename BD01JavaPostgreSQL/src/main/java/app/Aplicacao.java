069781169961e847ebbc8078c4b37dfb769ae7ff
Vgoddard <vinicius.goddard@gmail.com>
2023-09-14 18:27:46 -0300

    a

--- BD01JavaPostgreSQL/src/main/java/app/Aplicacao.java
+++ BD01JavaPostgreSQL/src/main/java/app/Aplicacao.java
@@ -1,55 +1,30 @@
 package app;
 
-import java.util.List;
+import static spark.Spark.*;
+import service.ProdutoService;
 
-import dao.DAO;
-import dao.UsuarioDAO;
-import model.Usuario;
 
 public class Aplicacao {
 	
-	public static void main(String[] args) throws Exception {
-		
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		
-		System.out.println("\n\n==== Inserir usuário === ");
-		Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
-		if(usuarioDAO.insert(usuario) == true) {
-			System.out.println("Inserção com sucesso -> " + usuario.toString());
-		}
-		
-		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "pablo"));
-			
-		System.out.println("\n\n==== Mostrar usuários do sexo masculino === ");
-		List<Usuario> usuarios = usuarioDAO.getSexoMasculino();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
+	private static ProdutoService produtoService = new ProdutoService();
+	
+    public static void main(String[] args) {
+        port(6789);
+        
+        staticFiles.location("/public");
+        
+        post("/produto/insert", (request, response) -> produtoService.insert(request, response));
 
-		System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
-		usuario.setSenha(DAO.toMD5("pablo"));
-		usuarioDAO.update(usuario);
-		
-		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", DAO.toMD5("pablo")));		
-		
-		System.out.println("\n\n==== Invadir usando SQL Injection ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "x' OR 'x' LIKE 'x"));
+        get("/produto/:id", (request, response) -> produtoService.get(request, response));
+        
+        get("/produto/list/:orderby", (request, response) -> produtoService.getAll(request, response));
 
-		System.out.println("\n\n==== Mostrar usuários ordenados por código === ");
-		usuarios = usuarioDAO.getOrderByCodigo();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
-		
-		System.out.println("\n\n==== Excluir usuário (código " + usuario.getCodigo() + ") === ");
-		usuarioDAO.delete(usuario.getCodigo());
-		
-		System.out.println("\n\n==== Mostrar usuários ordenados por login === ");
-		usuarios = usuarioDAO.getOrderByLogin();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
-	}
+        get("/produto/update/:id", (request, response) -> produtoService.getToUpdate(request, response));
+        
+        post("/produto/update/:id", (request, response) -> produtoService.update(request, response));
+           
+        get("/produto/delete/:id", (request, response) -> produtoService.delete(request, response));
+
+             
+    }
 }
